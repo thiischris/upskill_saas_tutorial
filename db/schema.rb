696bdaad3271717b5fad0444d1145834d60f6249
Rob Dey <christianclaustro@gmail.com>
2017-02-03 20:27:46 +0000

    Re created migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123070231) do
+ActiveRecord::Schema.define(version: 20170203200458) do
 
   create_table "contacts", force: :cascade do |t|
   end
